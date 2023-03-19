38e76303af8782d3cbe2a407c562adffa2964ae4
vaheaslanyan <vahe.sde@gmail.com>
2023-03-19 12:31:34 -0500

    Refactor webconfig

--- src/main/java/com/urartusoftware/urartusoftwarecontactservice/configs/WebConfig.java
+++ src/main/java/com/urartusoftware/urartusoftwarecontactservice/configs/WebConfig.java
@@ -1,5 +1,6 @@
 package com.urartusoftware.urartusoftwarecontactservice.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,8 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${deployment.ip.ui.local}")
+    private String ipLocal;
+
+    @Value("${deployment.ip.ui.deployed}")
+    private String ipDeployed;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins(ipLocal, ipDeployed);
     }
 }
