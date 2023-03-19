f4404f5e53b6342b1adcc735d883907bd84b6abb
vaheaslanyan <vahe.sde@gmail.com>
2023-03-19 13:57:18 -0500

    Modify webconfig

--- src/main/java/com/urartusoftware/urartusoftwarecontactservice/configs/WebConfig.java
+++ src/main/java/com/urartusoftware/urartusoftwarecontactservice/configs/WebConfig.java
@@ -18,6 +18,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins(ipLocal, ipDeployed);
     }
 }
